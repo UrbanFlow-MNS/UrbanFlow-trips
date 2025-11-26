e64ebcd3bf5ef5eacd93c13a819df19e3222e6e8
mathiasv2 <dumas.mathias@outlook.com>
2025-11-26 15:07:03 +0100

    feat: /create and /get routes

---
+++ UrbanFlow-trips/Controllers/RoutesController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using UrbanFlow_trips.DTO;
+using UrbanFlow_trips.Repository;
+
+namespace UrbanFlow_trips.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RoutesController : Controller
+{
+    private readonly RoutesRepository _routesRepository;
+    
+    public RoutesController(RoutesRepository routesRepository)
+    {
+        _routesRepository = routesRepository;
+    }
+    
+    [HttpPost("/create")]
+    public async Task<IActionResult> CreateAgency(CreateRouteDTO routeDTO)
+    {
+        await _routesRepository.CreateRouteAsync(routeDTO);
+        return Ok(new
+        {
+            message = "Agency created successfully"
+        });
+    }
+
+    [HttpGet("/all")]
+    public async Task<IActionResult> GetAllAgencies()
+    {
+        return Ok(await _routesRepository.GetAllRoutesAsync());
+    }
+}
