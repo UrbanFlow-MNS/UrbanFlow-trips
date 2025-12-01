5e4fb0e7a3be50aae7b7c47fb30740b347aa7a5d
mathiasv2 <dumas.mathias@outlook.com>
2025-12-01 15:28:28 +0100

    fix: new db scripts

---
+++ UrbanFlow-trips/Migrations/20251201142806_AddAgencyToStops.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrbanFlow_trips.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAgencyToStops : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AgencyId",
+                table: "Stops",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stops_AgencyId",
+                table: "Stops",
+                column: "AgencyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Stops_Agencies_AgencyId",
+                table: "Stops",
+                column: "AgencyId",
+                principalTable: "Agencies",
+                principalColumn: "AgencyId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Stops_Agencies_AgencyId",
+                table: "Stops");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Stops_AgencyId",
+                table: "Stops");
+
+            migrationBuilder.DropColumn(
+                name: "AgencyId",
+                table: "Stops");
+        }
+    }
+}
