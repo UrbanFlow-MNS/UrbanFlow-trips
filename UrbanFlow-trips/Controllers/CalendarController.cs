30e03dd3802bc7636a78b0166eacd30fcdd036e8
mathiasv2 <dumas.mathias@outlook.com>
2025-12-01 11:20:10 +0100

    feature: calendar controller

---
+++ UrbanFlow-trips/Controllers/CalendarController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using UrbanFlow_trips.DTO;
+using UrbanFlow_trips.Repository;
+
+namespace UrbanFlow_trips.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CalendarController : Controller
+{
+    private readonly CalendarRepository _calendarRepository;
+    public CalendarController(CalendarRepository calendarRepository)
+    {
+        _calendarRepository = calendarRepository;
+    }
+    
+    [HttpPost("create")]
+    public async Task<IActionResult> CreateCalendar(CreateCalendarDTO calendarDto)
+    {
+        await _calendarRepository.CreateCalendarAsync(calendarDto);
+        return Ok(new
+        {
+            message = "Calendar created successfully"
+        });
+    }
+    
+    [HttpGet("all")]
+    public async Task<IActionResult> GetAllCalendars()
+    {
+        return Ok(await _calendarRepository.GetAllCalendarsAsync());
+    }
+    
+}
