d683190f039180d16cb335dbd01706e6032c02a2
mathiasv2 <dumas.mathias@outlook.com>
2025-12-08 14:17:37 +0100

    fix: rmq testing

--- UrbanFlow-trips/Program.cs
+++ UrbanFlow-trips/Program.cs
@@ -10,11 +10,13 @@ using IConnection? connection = await factory.CreateConnectionAsync();
 using IChannel channel = await connection.CreateChannelAsync();
 
 await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+await channel.ExchangeDeclareAsync(exchange: "hello", type: ExchangeType.Fanout);
 
 const string message = "Hello World!";
 byte[] body = Encoding.UTF8.GetBytes(message);
 
-await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello", body: body);
+
+await channel.BasicPublishAsync(exchange: "hello", routingKey: "hello", body: body);
 Console.WriteLine(" [x] Sent {0}", message);
 
 
