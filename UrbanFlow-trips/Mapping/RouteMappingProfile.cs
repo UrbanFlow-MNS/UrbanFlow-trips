3c48297faaf9466169c905207a29b7c3cae01943
mathiasv2 <dumas.mathias@outlook.com>
2025-12-16 11:17:15 +0100

    implementing automapper

---
+++ UrbanFlow-trips/Mapping/RouteMappingProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using UrbanFlow_trips.DTO;
+using UrbanFlow_trips.Models;
+
+namespace UrbanFlow_trips.Mapping;
+
+public class RouteMappingProfile : Profile
+{ 
+    public RouteMappingProfile()
+    {
+        CreateMap<CreateRouteDTO, Routes>();
+        CreateMap<Routes, GetRouteDTO>();
+    }
+}
--- UrbanFlow-trips/Program.cs
+++ UrbanFlow-trips/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using RabbitMQ.Client;
 using UrbanFlow_trips.Database;
 using UrbanFlow_trips.DTO;
+using UrbanFlow_trips.Mapping;
 using UrbanFlow_trips.Options;
 using UrbanFlow_trips.Repository;
 using UrbanFlow_trips.Service;
@@ -11,17 +12,26 @@ using UrbanFlow_trips.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configuration des mappers
+builder.Services.AddAutoMapper(
+    cfg => {}, 
+    typeof(RouteMappingProfile)
+);
+
+// Configuration RabbitMQ
 builder.Services.Configure<RabbitMQOptions>(builder.Configuration.GetSection("RabbitMq"));
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+
+// Configuration Dbcontext
 builder.Services.AddDbContext<TripsDbContext>(options =>
     options.UseNpgsql(connectionString));
 
+// Injection de dépendances des repositories
 builder.Services.AddScoped<AgencyRepository>();
 builder.Services.AddScoped<RoutesRepository>();
 builder.Services.AddScoped<RouteTypeRepository>();
 builder.Services.AddScoped<StopRepository>();
-
 builder.Services.AddScoped<IRabbitMQService, RabbitMQService>();
 
 
@@ -30,6 +40,8 @@ builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
+
+// Update de la ddb à partir de la dernière migration
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
