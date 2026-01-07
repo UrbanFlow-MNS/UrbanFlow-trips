3aa03a11e5ebba352e6a7801d8732eae608f09ae
mathiasv2 <dumas.mathias@outlook.com>
2026-01-07 10:32:59 +0100

    feat: validators for updates

---
+++ UrbanFlow-trips/Application/Validators/UpdateRouteDTOValidator.cs
@@ -0,0 +1,20 @@
+using FluentValidation;
+using UrbanFlow_trips.DTO;
+
+namespace UrbanFlow_trips.Application.Validators;
+
+public class UpdateRouteDTOValidator : AbstractValidator<UpdateRouteDTO>
+{
+    public UpdateRouteDTOValidator()
+    {
+        RuleFor(x => x.RouteShortName)
+            .MaximumLength(50).WithMessage("Short name must be less than 50 characters")
+            .NotEmpty().WithMessage("Can't be null");
+        RuleFor(x => x.RouteLongName)
+            .MaximumLength(100).WithMessage("Name of city can't be superior to 100 characters")
+            .NotEmpty().WithMessage("Can't be null");
+        RuleFor(x => x.RouteTypeId)
+            .GreaterThan(0)
+            .NotEmpty().WithMessage("Must be greater than 0");
+    }
+}
