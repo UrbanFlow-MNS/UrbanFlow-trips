31daf19c59c316c46e208fba3eb6486ff783f0da
mathiasv2 <dumas.mathias@outlook.com>
2026-04-20 12:13:09 +0200

    feat: refactor routesrepo to always return routetype

--- UrbanFlow-trips/Application/DTO/Route/GetRouteDto.cs
+++ UrbanFlow-trips/Application/DTO/Route/GetRouteDto.cs
@@ -5,7 +5,7 @@ public class GetRouteDto
     public int RouteId { get; set; }
     public int AgencyId { get; set; }
     
-    public int RouteTypeName { get; set; }
+    public string RouteTypeName { get; set; }
 
     public string? RouteShortName { get; set; }
     public string? RouteLongName { get; set; }
