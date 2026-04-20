ada732e13cfe8d3ba374c3201b9702d519848734
mathiasv2 <dumas.mathias@outlook.com>
2026-04-20 11:50:58 +0200

    fix: return routetype for complete routes directly

--- UrbanFlow-trips/Application/DTO/Route/GetRouteDto.cs
+++ UrbanFlow-trips/Application/DTO/Route/GetRouteDto.cs
@@ -4,8 +4,8 @@ public class GetRouteDto
 {
     public int RouteId { get; set; }
     public int AgencyId { get; set; }
-    public int RouteTypeId { get; set; }
-
+    
+    public int RouteTypeName { get; set; }
 
     public string? RouteShortName { get; set; }
     public string? RouteLongName { get; set; }
