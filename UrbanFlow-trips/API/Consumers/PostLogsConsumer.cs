a38348bf27123cc42baf39ee6bce2ef625135956
mathiasv2 <dumas.mathias@outlook.com>
2026-04-25 19:03:26 +0200

    feat: try some configs

---
+++ UrbanFlow-trips/API/Consumers/PostLogsConsumer.cs
@@ -0,0 +1,14 @@
+using MassTransit;
+using UrbanFlow_trips.DTO;
+
+namespace UrbanFlow_trips.API.Consumers;
+
+public class PostLogsConsumer : IConsumer<LogMessageDto>
+{
+    public Task Consume(ConsumeContext<LogMessageDto> context)
+    {
+        Console.WriteLine("PostLogsConsumer");
+        Console.WriteLine(context.Message);
+        return Task.CompletedTask;
+    }
+}
