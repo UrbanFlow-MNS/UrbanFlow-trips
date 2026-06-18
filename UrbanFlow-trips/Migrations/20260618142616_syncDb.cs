d0ee87b4f5240f29847ed62c98d15aa3522c106d
mathiasv2 <dumas.mathias@outlook.com>
2026-06-18 16:27:06 +0200

    sync db

---
+++ UrbanFlow-trips/Migrations/20260618142616_syncDb.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace UrbanFlow_trips.Migrations
+{
+    /// <inheritdoc />
+    public partial class syncDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Incidents",
+                table: "Incidents");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IncidentId",
+                table: "Incidents",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Incidents",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Incidents",
+                table: "Incidents",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Incidents",
+                table: "Incidents");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Incidents");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IncidentId",
+                table: "Incidents",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Incidents",
+                table: "Incidents",
+                column: "IncidentId");
+        }
+    }
+}
